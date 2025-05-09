cf0356870dea8e5a2882820bf7a7d18602d0c523
seoyehyun <kingyehy@yonsei.ac.kr>
2025-05-09 18:12:29 +0900

    Add files via upload

---
+++ hw08/shFrag.glsl
@@ -0,0 +1,60 @@
+#version 300 es
+
+precision highp float;
+
+out vec4 FragColor;
+in vec3 fragPos;  
+in vec3 normal;  
+in vec2 texCoord;
+
+struct Material {
+    sampler2D diffuse; // diffuse map
+    vec3 specular;     // 표면의 specular color
+    float shininess;   // specular 반짝임 정도
+};
+
+struct Light {
+    //vec3 position;
+    vec3 direction;
+    vec3 ambient; // ambient 적용 strength
+    vec3 diffuse; // diffuse 적용 strength
+    vec3 specular; // specular 적용 strength
+};
+
+uniform Material material;
+uniform Light light;
+uniform vec3 u_viewPos;
+uniform int u_levels;
+
+float quantize(float value, int levels) {
+    float step = 1.0 / float(levels);
+    return floor(value / step) * step;
+}
+
+void main() {
+    // ambient
+    vec3 rgb = vec3(1.0, 0.5, 0.31);
+    vec3 ambient = light.ambient * rgb;
+  	
+    // diffuse 
+    vec3 norm = normalize(normal);
+    //vec3 lightDir = normalize(light.position - fragPos);
+    vec3 lightDir = normalize(light.direction);
+    float dotNormLight = dot(norm, lightDir);
+    float diff = max(dotNormLight, 0.0);
+    diff = quantize(diff, u_levels);
+    vec3 diffuse = light.diffuse * diff * rgb;  
+    
+    // specular
+    vec3 viewDir = normalize(u_viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = 0.0;
+    if (dotNormLight > 0.0) {
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    }
+    spec = quantize(spec, u_levels);
+    vec3 specular = light.specular * spec * material.specular;  
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+} 
